2d254b23cb124e78087dd152f5602a449b53b6be
keer-thi <56103306+keer-thi@users.noreply.github.com>
2020-04-16 11:39:52 +0530

    Add files via upload

---
+++ kk.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node*next;
+};
+
+void display(struct node*head){
+    struct node*current=head;
+    printf("traversing the list...\n");
+    while (current!=NULL){
+        printf("%d",current->data);
+        current=current->next;
+    }
+}
+
+void reverse_display(struct node*head){
+    if(head){
+        reverse_display(head->next);
+        printf("%d",head->data);
+    }
+}
+
+struct node*createnode(int d){
+    struct node * temp= (sizeof(struct node));
+    temp->data=d;
+    temp->next=NULL;
+    return temp;
+}
+
+int main(){
+    printf("cresting the linked list by inserting new nodes at the end\n");
+    printf("enter 0 to stop building the list,else enter any integer\n");
+    int k,count=1,x;
+    struct node* curr,*temp;
+    scanf("%d",&k);
+    struct node* head=createnode(k);
+    scanf("%d",&k);
+    temp=head;
+
+    while(k){
+        curr=createnode(k);
+        temp->next=curr;
+        temp=temp->next;
+        scanf("%d",&k);
+}
+
+display(head); 
+printf("\ndisplaying in reverse order...\n");
+reverse_display(head);
+return 0;
+}
+
